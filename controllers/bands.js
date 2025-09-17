de238aa9f431136193ff467542f7b1682d633adc
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-09-17 01:03:12 -0300

    Update: Respuesta del servidor en el controlador de la entidad bandas

--- controllers/bands.js
+++ controllers/bands.js
@@ -1,24 +1,37 @@
-bandas = [
-    {
-        id: 1,
-        nombre: 'Nombre 1'
-    },
-    {
-        id: 2,
-        nombre: 'Nombre 1'
-    },
-]
+const bandas = require('../db/index.js');
+
+function obtenerIdYNombre(array) {
+    return array.map(banda => ({
+        id: banda.id,
+        nombre: banda.nombre
+    }))
+}
+
+function buscarBanda(id) {
+    return bandas.lista.find(banda => banda.id == id);
+}
+
+const lista = obtenerIdYNombre(bandas.lista);
+
 
 const bandsController = {
     index: (req, res, next) => {
-        // traer a la vista (id + banda en un array)
-        res.render('bands', { bandas: bandas});
+        res.render('bands', { bandas: lista });
     },
     show: (req, res, next) => {
         const id = req.params.id;
-        // validar id si existe
-        // traer (informacion de la banda con todos sus datos)
-        res.send(`Mostrar banda segun ID: ${id}`);
+        const banda = buscarBanda(id);
+        
+        if (!banda) {
+            res.status(404).send('Recurso no encontrado');
+        }
+
+        res.send(`
+            Mostrar banda segun ID: ${id}, <br>
+            Nombre: ${banda.nombre}, <br>
+            Integrantes: ${banda.integrantes}, <br>
+            Genero: ${banda.genero}
+        `);
     }
 }
 
